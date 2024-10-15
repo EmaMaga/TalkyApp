9a8c6ad778c0b680ff495a89de789cbcf6e53c7e
Emanuel Magallanes <ema_magallanes@hotmail.com>
2024-10-15 17:07:07 -0300

    Adding initilzr

---
+++ src/main/java/Emanuel/TalkyApp/TalkyAppApplication.java
@@ -0,0 +1,13 @@
+package Emanuel.TalkyApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalkyAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Emanuel/TalkyApp/TalkyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package Emanuel.TalkyApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
